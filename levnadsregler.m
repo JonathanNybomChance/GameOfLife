ba29c87c7c6cc1672d78fd4bbd48bc00bb2a1c7a
bb2100 <95761437+bb2100@users.noreply.github.com>
2023-12-05 11:15:41 +0100

    Add files via upload

--- levnadsregler.m
+++ levnadsregler.m
@@ -1,5 +1,22 @@
-A = [randi(0:1,16)]
+B = [randi(0:1,16)]
 
-function villkor = levnadsregler
-antal = antalgrannar(A,2,3)
+antal = antalgrannar(B,2,3)
 
+villkor = vnadsregler(antal)
+
+
+function value = vnadsregler(antal)
+%antal = talgrannar(A,2,3)
+
+    if antal < 2
+        value = 0
+
+    elseif antal > 3
+        value = 0
+
+
+    elseif antal == 3
+        value = 1
+
+    end
+end
